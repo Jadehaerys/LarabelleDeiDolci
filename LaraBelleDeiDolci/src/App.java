3dd9fa6462aaefed905c04ce1ba36a37e2b6b276
unknown <jmykelvenz@gmail.com>
2024-11-28 00:12:01 +0800

    added logo title

--- LaraBelleDeiDolci/src/App.java
+++ LaraBelleDeiDolci/src/App.java
@@ -1,3 +1,4 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import dasboard.Dashboard;
@@ -6,15 +7,14 @@ public class App {
     public static void main(String[] args) {
     JFrame frame = new JFrame("Larabelle Dei Dolci");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 800); // Set the frame size to match the panel dimensions
+        frame.setSize(1000, 800); 
+        ImageIcon img = new ImageIcon("Images\\Lorebelle-removebg-preview.png");
+        frame.setIconImage(img.getImage());
         
-        // Initialize the Dashboard
         Dashboard dashboard = new Dashboard();
         
-        // Add the main panel of the Dashboard to the JFrame
         frame.setContentPane(dashboard.getPanel());
 
-        // Make the JFrame visible
         frame.setVisible(true);
 
 
